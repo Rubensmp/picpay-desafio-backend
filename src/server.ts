dd7dc29eb4d96c9b91e2009efff57efea715a3f0
rubensmp <rubens.mp1@gmail.com>
2024-05-26 01:39:18 -0300

    feat: add docs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,37 @@
 import fastify from "fastify";
 
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
+import fastifySwagger from "@fastify/swagger"
+import fastifySwaggerUI from "@fastify/swagger-ui"
+import { fastifyCors } from "@fastify/cors";
+
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
 import { registerForUser } from "./routes/register-user";
 import { tranferToUser } from "./routes/transfer";
 import { refoundToUser } from "./routes/refound";
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  // http://meufrontend.com
+  origin: '*',
+})
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: "event.Api",
+      description: "Especificações da API para o back-end da aplicação event.Api.",
+      version: '1.0.0'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs'
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
